938018a5ddafa8306264a6b2a56c2a1409727b6a
vikramarsid <vikramarsid@gmail.com>
2018-07-26 09:51:10 -0400

    Added python 3 test support.

--- html2jirawiki/__init__.py
+++ html2jirawiki/__init__.py
@@ -1,3 +1,7 @@
 # coding=utf-8
+import sys
 
-from html2jirawiki import html_to_jira_wiki, ATX, ATX_CLOSED
+if sys.version_info > (3, 0):
+    from html2jirawiki.html2jirawiki import html_to_jira_wiki, ATX, ATX_CLOSED
+else:
+    from html2jirawiki import html_to_jira_wiki, ATX, ATX_CLOSED
